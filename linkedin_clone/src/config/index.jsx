0634f2fac2fb295c95cb9c03e0fc9dc70d8b2e4b
ramananda <ramanandasagar3670@gmail.com>
2025-06-11 14:49:53 +0530

    image correction

--- linkedin_clone/src/config/index.jsx
+++ linkedin_clone/src/config/index.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const BASE_URL ="http://localhost:8011"
+export const BASE_URL ="https://linkedin-clone-58q0.onrender.com"
 
 export const clientServer = axios.create({
   baseURL: BASE_URL
